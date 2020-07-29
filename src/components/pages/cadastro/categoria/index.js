6e44ef4240e415b477c96c730951dab7e0b44c60
Gabriel Figueira <gabrielfigueira98@gmail.com>
2020-07-28 23:35:43 -0400

    Day 2

--- src/components/pages/cadastro/categoria/index.js
+++ src/components/pages/cadastro/categoria/index.js
@@ -1,16 +1,32 @@
 import React from 'react';
-import PageDefault from '../../../PageDefault'
 import { Link } from 'react-router-dom';
+import PageDefault from '../../../PageDefault';
 
-function CadastroCategoria(){
-  return(
+function CadastroCategoria() {
+  return (
     <PageDefault>
       <h1>Category Register</h1>
+
+      <form>
+
+        <label>
+          Category name:
+          <input
+            type="text"
+          />
+        </label>
+
+        <button>
+          Register
+        </button>
+      </form>
+
+
       <Link to="/">
         Return to home
       </Link>
     </PageDefault>
-  );
+  )
 }
 
 export default CadastroCategoria;
