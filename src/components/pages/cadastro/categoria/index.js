e47a47e48e2562371cf759885326c54437da2a25
Gabriel Figueira <gabrielfigueira98@gmail.com>
2020-07-30 21:03:21 -0400

    bug correction

--- src/components/pages/cadastro/categoria/index.js
+++ src/components/pages/cadastro/categoria/index.js
@@ -29,7 +29,7 @@ function CadastroCategoria() {
 
   useEffect(() => {
     if (window.location.href.includes('localhost')) {
-      const URL = window.location.hostname.includes('http://localhost:8080/categorias')
+      const URL = window.location.hostname.includes('localhost')
       ? 'http://localhost:8080/categorias'
       : 'https://metalflix.herokuapp.com/categorias';
       fetch(URL)
